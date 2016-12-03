fd2e389bb300d3849460cbcf60c0c7c90ca09d4c
DanWilkes02 <daniel_wilkes@outlook.com>
2016-12-03 12:34:55 +0000

    Basic stuff
    Adding stuff, basic image selection.

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,22 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Dan Wilkes on 03/12/2016.
+//  Copyright © 2016 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+   //MARK: Initialisation
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    
+    
+    
+}
